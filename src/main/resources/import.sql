a9684c7b9fd24c3ee17a08c8bc3c7651fc580065
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-12-17 21:31:46 -0300

    carga de dados na base de Animais

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,8 @@ INSERT INTO raca (nome) values ('Cachorro');
 INSERT INTO raca (nome) values ('Gato');
 INSERT INTO raca (nome) values ('Cavalo');
 INSERT INTO raca (nome) values ('Tartaruga');
+
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Spike','Enzo','Simples','99 99999-9999','2023-01-01',1);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Thor','Marcos','Epico','99 99999-9999','2023-06-21',2);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Trovao', 'Samuel','Comum','99 99999-9999','2023-06-21',3);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Relampago','Antonio','Anciao','99 99999-9999','2023-12-23',4);
