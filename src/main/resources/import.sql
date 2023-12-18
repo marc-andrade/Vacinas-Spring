05621641fc18b4d6833ba17eeeacd55ad3182d2a
Marcos Andrade <marcos_q_andrade@hotmail.com>
2023-12-17 21:32:56 -0300

    carga de dados na base de Animais

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,7 +3,7 @@ INSERT INTO raca (nome) values ('Gato');
 INSERT INTO raca (nome) values ('Cavalo');
 INSERT INTO raca (nome) values ('Tartaruga');
 
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Spike','Enzo','Simples','99 99999-9999','2023-01-01',1);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Thor','Marcos','Epico','99 99999-9999','2023-06-21',2);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Trovao', 'Samuel','Comum','99 99999-9999','2023-06-21',3);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Relampago','Antonio','Anciao','99 99999-9999','2023-12-23',4);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Spike','Enzo','99 99999-9999','Simples','2023-01-01',1);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Thor','Marcos','99 99999-9999','Epico','2023-06-21',2);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Trovao', 'Samuel','99 99999-9999','Comum','2023-06-21',3);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Relampago','Antonio','99 99999-9999','Anciao','2023-12-23',4);
