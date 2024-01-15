13cbafeebc53743efdcc371b822d72144e2318a5
Marcos Andrade <marcos_q_andrade@hotmail.com>
2024-01-15 17:01:38 -0300

    ajustes nos imports solicitados pelo professor fabio

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,7 @@ INSERT INTO raca (id, nome) VALUES (5, 'Siamese cat');
 INSERT INTO raca (id, nome) VALUES (6, 'Persian cat');
 INSERT INTO raca (id, nome) VALUES (7, 'Russian Blue');
 
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Don','Diego','Diego','Cachorro','2015-07-01',2);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Don','Diego','912345678','Cachorro','2015-07-01',2);
 INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Tel', 'Alessandra', '945678123','Cachorro','2014-10-07',2);
 INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Petrus', 'Augusto', '956781234','Cachorro','2023-06-21',1);
 INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Viseu', 'Diego', '912345678','Gato','2015-07-01',7);
