16cbf3b172cd460f5e317f4e7f5bba9c8f9b1bb1
Marcos Andrade <marcos_q_andrade@hotmail.com>
2024-01-15 01:39:20 -0300

    ajustes nos imports solicitados pelo professor fabio

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,14 @@
-INSERT INTO raca (nome) values ('Cachorro');
-INSERT INTO raca (nome) values ('Gato');
-INSERT INTO raca (nome) values ('Cavalo');
-INSERT INTO raca (nome) values ('Tartaruga');
+INSERT INTO raca (id, nome) VALUES (1, 'Pug');
+INSERT INTO raca (id, nome) VALUES (2, 'Boxer');
+INSERT INTO raca (id, nome) VALUES (3, 'Shih Tzu');
+INSERT INTO raca (id, nome) VALUES (4, 'Dachshund');
+INSERT INTO raca (id, nome) VALUES (5, 'Siamese cat');
+INSERT INTO raca (id, nome) VALUES (6, 'Persian cat');
+INSERT INTO raca (id, nome) VALUES (7, 'Russian Blue');
 
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Spike','Enzo','99 99999-9999','Simples','2023-01-01',1);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Thor','Marcos','99 99999-9999','Epico','2023-06-21',2);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Trovao', 'Samuel','99 99999-9999','Comum','2023-06-21',3);
-INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Relampago','Antonio','99 99999-9999','Anciao','2023-12-23',4);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Don','Diego','Diego','Cachorro','2015-07-01',2);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Tel', 'Alessandra', '945678123','Cachorro','2014-10-07',2);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Petrus', 'Augusto', '956781234','Cachorro','2023-06-21',1);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Viseu', 'Diego', '912345678','Gato','2015-07-01',7);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Duelo', 'Alessandra', '945678123','Gato','2014-10-07',6);
+INSERT INTO animal (nome,dono,telefone,tipo,nascimento,raca) values ('Gargamel', 'Augusto', '956781234','Gato','2016-01-05',5);
